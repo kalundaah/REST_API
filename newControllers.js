ee240392c29e8141557844a97a57a59654c05b59
kalundaah <kalundaneville@gmail.com>
2023-04-14 11:01:16 +0300

    created the proper schema

--- newControllers.js
+++ newControllers.js
@@ -13,7 +13,7 @@ const getClients = (req, res)=>{
 };
 //This checks for the string 'clientId' in the body of the postman and takes in new data to the json
 const createClient = (req, res) =>{
-    const {clientId} = req.body;
+    const clientId = req.body;
     if(!clientId){ //if it is empty return 400 invalid request
         return res.status(400).json({success: false, message: 'Please provide an ID'});
     } //if it has data change the data to string notation then push to the json
@@ -29,7 +29,7 @@ const getRooms = (req, res)=>{
 
 //This checks for the string 'roomId' in the body of the postman and takes in new data to the json
 const createRoom = (req, res) =>{
-    const {roomId} = req.body;
+    const roomId = req.body;
     if(!roomId){ //if it is empty return 400 invalid request
         return res.status(400).json({success: false, message: 'Please provide an ID'});
     } //if it has data change the data to string notation then push to the json
@@ -44,16 +44,24 @@ const getBookings = (req, res)=>{
 };
 //This checks for the string both 'roomId' and  'clientId' in the body of the postman and takes in new data to the json
 const createBooking = (req, res) =>{
-    const {clientId, roomId} = req.body;
-    if(!clientId || !roomId){ //if it is empty return 400 invalid request
+    const {clientId, roomId,checkin,checkout,checkouttime} = req.body;
+    if(!clientId || !roomId || !checkout || !checkouttime){ //if it is empty return 400 invalid request
         return res.status(400).json({success: false, message: 'Please provide an ID'});
     } //if it has data change the data to string notation then push to the json
     dataObj["clients"].push(req.body.clientId);
     dataObj["rooms"].push(req.body.roomId);
+    const date = new Date();
+    const tddat = date.getFullYear()+'-'+(date.getMonth()+1)+'-'+date.getDate();
+    const tdtim = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    dataObj["checkin"].push(tddat);
+    dataObj["checkintime"].push(tdtim);
+    dataObj["checkout"].push(req.body.checkout);
+    dataObj["checkouttime"].push(req.body.checkouttime);
     const book = req.body.roomId.toString() + req.body.clientId.toString(); //the roomId and clientId have to turn to a string in order to concatunate
     dataObj["bookings"].push(book);
     const data = JSON.stringify(dataObj);
     fs.writeFileSync('./data.json', data + "\n",{flag: "a+"});
+    console.log(`${clientId} has been added at this date ${tddat} and time ${tdtim} `);
     res.status(201).json({success: true, data: dataObj["bookings"]});
 };
 //This fetches the checkin data from the json
@@ -61,13 +69,13 @@ const getCheckin = (req, res)=>{
     res.status(200).json({success: true, data: dataObj["checkin"]});
 };
 const createCheckin = (req, res) =>{
-    const {checkin} = req.body;
-    if(!checkin){ //if it is empty return 400 invalid request
+    const checkIn = req.body;
+    if(!checkIn){ //if it is empty return 400 invalid request
         return res.status(400).json({success: false, message: 'Please provide a check in'});
     } //if it has data change the data to string notation then push to the json
     dataObj["checkin"].push(req.body);
     const data = JSON.stringify(dataObj);
-    fs.writeFileSync('./data.json', data + "\n",{flag: "a+"});
+    fs.writeFileSync('./data.json', data + "\n",{flag: "a"});
     res.status(201).json({success: true, data: dataObj["checkin"]});
 };
 //This fetches the checkin data from the json
@@ -75,7 +83,7 @@ const getCheckout = (req, res)=>{
     res.status(200).json({success: true, data: dataObj["checkout"]});
 };
 const createCheckout = (req, res) =>{
-    const {checkout} = req.body;
+    const checkout = req.body;
     if(!checkout){ //if it is empty return 400 invalid request
         return res.status(400).json({success: false, message: 'Please provide a check in'});
     } //if it has data change the data to string notation then push to the json
@@ -89,7 +97,7 @@ const getChecktime = (req, res)=>{
     res.status(200).json({success: true, data: dataObj["Checktime"]});
 };
 const createChecktime = (req, res) =>{
-    const {Checktime} = req.body;
+    const Checktime = req.body;
     if(!Checktime){ //if it is empty return 400 invalid request
         return res.status(400).json({success: false, message: 'Please provide a check in'});
     } //if it has data change the data to string notation then push to the json
@@ -112,5 +120,5 @@ module.exports = {
     getCheckout,
     createCheckout,
     getChecktime,
-    createChecktime
+    createChecktime,
 }
